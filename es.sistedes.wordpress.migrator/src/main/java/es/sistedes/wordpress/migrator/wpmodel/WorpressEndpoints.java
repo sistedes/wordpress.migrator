2fa0f63875a62d325659cd289bad2aeee732ba53
Abel Gómez <abgolla@gmail.com>
2023-09-07 21:16:38 +0200

    Get more than 10 results from the wordpress endpoints

--- es.sistedes.wordpress.migrator/src/main/java/es/sistedes/wordpress/migrator/wpmodel/WorpressEndpoints.java
+++ es.sistedes.wordpress.migrator/src/main/java/es/sistedes/wordpress/migrator/wpmodel/WorpressEndpoints.java
@@ -3,7 +3,7 @@ package es.sistedes.wordpress.migrator.wpmodel;
 final class WorpressEndpoints {
 	
 	static final String LIBRARY_ENDPOINT = "/wp-json/wp/v2/biblioteca";
-	static final String LIBRARY_PARENT_QUERY = "?parent=%s";
+	static final String LIBRARY_PARENT_QUERY = "?parent=%s&per_page=100";
 	static final String ARTICLE_ENDPOINT = "/wp-json/wp/v2/articulo/%s";
 
 	private WorpressEndpoints() {
