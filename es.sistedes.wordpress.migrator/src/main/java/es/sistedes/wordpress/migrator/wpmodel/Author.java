2778c75c6a9b1fbe249a5e333da53ac4c6609a44
Abel Gómez <abgolla@gmail.com>
2023-09-08 03:12:15 +0200

    Attempt to separate countries with comma rather than hyphen

--- es.sistedes.wordpress.migrator/src/main/java/es/sistedes/wordpress/migrator/wpmodel/Author.java
+++ es.sistedes.wordpress.migrator/src/main/java/es/sistedes/wordpress/migrator/wpmodel/Author.java
@@ -14,6 +14,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.RegExUtils;
 import org.apache.commons.lang3.StringUtils;
 
 public class Author {
@@ -98,7 +99,7 @@ public class Author {
 	
 	public Author(String fullname, String email, String affiliation) {
 		this.email = StringUtils.trimToNull(email);
-		this.affiliation = StringUtils.trimToNull(affiliation);
+		this.affiliation = RegExUtils.replaceAll(StringUtils.trimToNull(affiliation), " - ([\\w\\s]+?)$", ", $1");
 		parseName(fullname);
 	}
 
